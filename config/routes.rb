0212fdee2a5921719905d5a35fa909a333ea5863
George F Murphy <george@matmon.com>
2012-02-03 15:17:58 -0600

    udpate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
-Rails.application.routes.draw do
-  # Add your extension routes here
-  #get "quick_order/index"
-  #post "quick_order/sku_populate_cart"
-
+Spree::Core::Engine.routes.append do
   match "quick-order" => "quick_order#index", :via => :get
-  match "quick-order" => "quick_order#sku_populate_cart", :via => :post, :as => "sku_populate_cart"
-
+  match "quick-order" => "quick_order#sku_populate_cart", :via => :post,
+    :as => "sku_populate_cart"
 end
