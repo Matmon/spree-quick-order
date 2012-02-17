26f44ae07b1f01a26ee011304bfb00930239dce9
George F Murphy <george@matmon.com>
2012-02-17 12:34:02 -0600

    use Spree::Variant for lookup.  format code

--- lib/spree_quick_order/bulk_populate_order.rb
+++ lib/spree_quick_order/bulk_populate_order.rb
@@ -1,52 +1,46 @@
 module SpreeQuickOrder
   class BulkPopulateOrder
     include ActionView::Helpers::TagHelper
-    
+
     # Accepts the current spree order object and the rows sku params
     # checks if each sku is valid and adds to order
     # otherwise sets error messages
 
     def initialize(order, cartrows)
-      
       @rows = Hash[Array(cartrows)]
-
       @error_messages = []
-      
-      @rows.delete_if{ |k,v| v[:sku].empty? }
-      
-      if @rows.size > 0 
-        
+      @rows.delete_if { |k,v| v[:sku].empty? }
+
+      if @rows.size > 0
         @rows.each do |k, v|
           variant_sku = v[:sku].strip
           quantity = v[:quantity].to_i
 
-          product_variant = Variant.find_by_sku(variant_sku) # unless variant_sku.empty? ## should no longer be empty since we delete empty rows above
-
+          # unless variant_sku.empty?
+          ## should no longer be empty since we delete empty rows above
+          product_variant = Spree::Variant.find_by_sku(variant_sku)
           v[:variant] = product_variant
-         
+
           if product_variant.nil?
             v[:error] = true
-            @error_messages << "SKU Not Found: #{variant_sku}" 
+            @error_messages << "SKU Not Found: #{variant_sku}"
           end
 
           if quantity < 1
             v[:error] = true
-            @error_messages << "Enter a Valid Quantity for SKU: #{variant_sku}" 
+            @error_messages << "Enter a Valid Quantity for SKU: #{variant_sku}"
           end
 
-        end 
-        
+        end
+
         ## no errors add product variants to order
         @rows.each do |k , v|
           order.add_variant(v[:variant], v[:quantity].to_i)
         end unless self.has_error_messages?
 
-      else 
+      else
         @error_messages << "You Must Enter at least one Product SKU"
       end
-      
-      # @bulk_order = order 
-
     end
 
     def rows
@@ -74,6 +68,5 @@ module SpreeQuickOrder
     def has_error_messages?
       return !@error_messages.empty?
     end
-
   end
 end
